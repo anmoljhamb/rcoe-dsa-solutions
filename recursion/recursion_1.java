d7f51922cfb4cd0d9983bccfb286a7ff3d94064d
Anmol Jhamb <talktoanmol@outlook.com>
2024-07-16 11:38:22 +0530

    Math.pow

--- recursion/recursion_1.java
+++ recursion/recursion_1.java
@@ -2,7 +2,8 @@ class recursion_1 {
   public static void main(String[] args) {
     int n = 5;
     // System.out.println("The factorial is: " + factorial(n));
-    print(n);
+    // print(n);
+    System.out.println("The value of 2^3: " + pow(2, 3));
   }
 
   public static int factorial(int n) {
@@ -18,5 +19,11 @@ class recursion_1 {
     System.out.println(n);
     print(n - 1);
     System.out.println(n);
+  }
+
+  public static int pow(int a, int b) {
+    if (b == 0)
+      return 1;
+    return a * pow(a, b - 1);
   }
 }
