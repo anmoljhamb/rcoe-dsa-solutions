1d87d81cb96d90c88af8a98cd8ba0310b9904715
Anmol Jhamb <talktoanmol@outlook.com>
2024-07-16 15:30:44 +0530

    linear search

--- arrays/array_1.java
+++ arrays/array_1.java
@@ -7,21 +7,31 @@ class array_1 {
     // System.out.println(max(arr));
     int[] arr1 = { -1, -2, -3, 5, 5 };
     int[] arr2 = { 1, 2, 3 };
-    System.out.println("Before: ");
-    System.out.println("arr1: ");
-    printArr(arr1);
-    System.out.println("arr2: ");
-    printArr(arr2);
+    // System.out.println("Before: ");
+    // System.out.println("arr1: ");
+    // printArr(arr1);
+    // System.out.println("arr2: ");
+    // printArr(arr2);
+    //
+    // int[] temp = arr1;
+    // arr1 = arr2;
+    // arr2 = temp;
+    //
+    // System.out.println("After: ");
+    // System.out.println("arr1: ");
+    // printArr(arr1);
+    // System.out.println("arr2: ");
+    // printArr(arr2);
+    System.out.println("The first occurence of 1 is: " + search(arr2, 1));
+  }
 
-    int[] temp = arr1;
-    arr1 = arr2;
-    arr2 = temp;
-
-    System.out.println("After: ");
-    System.out.println("arr1: ");
-    printArr(arr1);
-    System.out.println("arr2: ");
-    printArr(arr2);
+  public static int search(int[] arr, int target) {
+    for (int i = 0; i < arr.length; i++) {
+      if (arr[i] == target) {
+        return i;
+      }
+    }
+    return -1;
   }
 
   public static void swap(int[] arr, int a, int b) {
