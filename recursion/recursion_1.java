ec290e7afa8958a0820ec836b391f5f94edf7743
Anmol Jhamb <talktoanmol@outlook.com>
2024-07-16 11:36:03 +0530

    print n to 1 and 1 to n

--- recursion/recursion_1.java
+++ recursion/recursion_1.java
@@ -1,12 +1,22 @@
 class recursion_1 {
   public static void main(String[] args) {
     int n = 5;
-    System.out.println("The factorial is: " + factorial(n));
+    // System.out.println("The factorial is: " + factorial(n));
+    print(n);
   }
 
   public static int factorial(int n) {
     if (n == 0 || n == 1)
       return 1;
     return n * factorial(n - 1);
+  }
+
+  public static void print(int n) {
+    if (n == 0) {
+      return;
+    }
+    System.out.println(n);
+    print(n - 1);
+    System.out.println(n);
   }
 }
