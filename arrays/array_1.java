6edc68f1429e3081417cd313a4e2532d306d6ea9
Anmol Jhamb <talktoanmol@outlook.com>
2024-07-16 14:42:42 +0530

    swap in array

--- arrays/array_1.java
+++ arrays/array_1.java
@@ -2,7 +2,15 @@ class array_1 {
   public static void main(String[] args) {
     int[] arr = { -1, -2, -3 };
     printArr(arr);
+    swap(arr, 0, 1);
+    printArr(arr);
     System.out.println(max(arr));
+  }
+
+  public static void swap(int[] arr, int a, int b) {
+    int temp = arr[a];
+    arr[a] = arr[b];
+    arr[b] = temp;
   }
 
   public static int max(int[] arr) {
