e36656a0ff9ee58d7f0cc9417b5dd78fa93ebcb1
Anmol Jhamb <talktoanmol@outlook.com>
2024-07-17 11:58:08 +0530

    coinToss 2

--- recursion/recursion_2.java
+++ recursion/recursion_2.java
@@ -28,4 +28,15 @@ class recursion_2 {
     coinToss(n - 1, ans + "H");
     coinToss(n - 1, ans + "T");
   }
+
+  public static void coinToss2(int n, String ans) {
+    if (n == 0) {
+      System.out.println(ans);
+      return;
+    }
+    if (ans.length() == 0 || ans.charAt(ans.length() - 1) != 'H') {
+      coinToss(n - 1, ans + "H");
+    }
+    coinToss(n - 1, ans + "T");
+  }
 }
