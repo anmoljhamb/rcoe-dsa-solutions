1386669026d25c4275fce46d81a263754d00f4b3
Anmol Jhamb <talktoanmol@outlook.com>
2024-07-18 11:03:57 +0530

    recursin_3

--- recursion/recursion_3.java
+++ recursion/recursion_3.java
@@ -1,7 +1,9 @@
 public class recursion_3 {
   public static void main(String[] args) {
     int n = 100;
-    printLexicalNumbers(n, 0);
+    for (int i = 1; i <= 9; i++) {
+      printLexicalNumbers(n, i);
+    }
   }
 
   public static void printLexicalNumbers(int n, int curr) {
@@ -11,12 +13,7 @@ public class recursion_3 {
 
     System.out.println(curr);
 
-    int i = 0;
-    if (curr == 0) {
-      i = 1;
-    }
-
-    for (i = 1; i <= 9; i++) {
+    for (int i = 1; i <= 9; i++) {
       printLexicalNumbers(n, curr * 10 + i);
     }
   }
