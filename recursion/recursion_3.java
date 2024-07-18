fdda77e55f86f77a7a467d6a021cb022f58ce134
Anmol Jhamb <talktoanmol@outlook.com>
2024-07-18 12:39:40 +0530

    print all strings

--- recursion/recursion_3.java
+++ recursion/recursion_3.java
@@ -1,6 +1,19 @@
 public class recursion_3 {
   public static void main(String[] args) {
-    printPath(3, 3, 0, 0, "");
+    // printPath(3, 3, 0, 0, "");
+    String str = "abcde";
+    printPermutations(str, "");
+  }
+
+  public static void printPermutations(String str, String ans) {
+    if (str.length() == 0) {
+      System.out.println(ans);
+      return;
+    }
+
+    for (int i = 0; i < str.length(); i++) {
+      printPermutations(str.substring(0, i) + str.substring(i + 1), ans + str.charAt(i));
+    }
   }
 
   public static void printPath(int m, int n, int i, int j, String ans) {
