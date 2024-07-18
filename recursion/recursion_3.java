5ad6a0abe10dd700af155c95d340cf03c540d7f5
Anmol Jhamb <talktoanmol@outlook.com>
2024-07-18 11:05:40 +0530

    store result in list

--- recursion/recursion_3.java
+++ recursion/recursion_3.java
@@ -1,20 +1,26 @@
+import java.util.ArrayList;
+
+import com.sun.tools.javac.util.List;
+
 public class recursion_3 {
   public static void main(String[] args) {
     int n = 100;
+    List<Integer> list = new ArrayList<Integer>();
+
     for (int i = 1; i <= 9; i++) {
-      printLexicalNumbers(n, i);
+      printLexicalNumbers(n, i, list);
     }
   }
 
-  public static void printLexicalNumbers(int n, int curr) {
+  public static void printLexicalNumbers(int n, int curr, java.util.List<Integer> list) {
     if (curr > n) {
       return;
     }
 
-    System.out.println(curr);
+    list.add(curr);
 
     for (int i = 1; i <= 9; i++) {
-      printLexicalNumbers(n, curr * 10 + i);
+      printLexicalNumbers(n, curr * 10 + i, list);
     }
   }
 }
