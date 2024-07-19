1a457348140693b784551ad346d0e175375ddb74
Anmol Jhamb <talktoanmol@outlook.com>
2024-07-19 15:05:11 +0530

    format file

--- arrays/insertion_sort.java
+++ arrays/insertion_sort.java
@@ -6,25 +6,19 @@ class insertion_sort {
     // sort(arr);
     print(arr);
 
-  
   }
-   
 
-  public static void fixLastElement(int[] arr, int i){
+  public static void fixLastElement(int[] arr, int i) {
     int item = arr[i];
-      int j = i-1;
+    int j = i - 1;
 
-  
-    while ( j >= 0 && arr[j] > item ){
-      arr[j+1] = arr[j];
+    while (j >= 0 && arr[j] > item) {
+      arr[j + 1] = arr[j];
       j--;
     }
-    arr[j+1] = item;
+    arr[j + 1] = item;
   }
 
-  
-
-    
   public static void print(int[] arr) {
     for (int i = 0; i < arr.length; i++) {
       System.out.print(arr[i] + " ");
