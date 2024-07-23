e32326d8fc10767758f02899b2a3108952c4c453
Anmol Jhamb <talktoanmol@outlook.com>
2024-07-23 11:20:57 +0530

    palindrome

--- recursion/partitions.java
+++ recursion/partitions.java
@@ -1,16 +1,32 @@
 public class partitions {
   public static void main(String[] args) {
-    String str = "aab";
+    String str = "nitin";
     generate(str, "");
   }
 
-  static void generate(String str, String ans) {
-    if (str.length() == 0) {
-      System.out.println(ans);
+  static boolean isPalindrome(String str) {
+    int left = 0;
+    int right = str.length() - 1;
+    while (left < right) {
+      if (str.charAt(left) != str.charAt(right)) {
+        return false;
+      }
+      left++;
+      right--;
+    }
+    return true;
+  }
+
+  
+  static void generate(String str, String ans){
+  if (str.length() == 0) {
+
+    System.out.println(ans);
       return;
     }
     for (int i = 0; i < str.length(); i++) {
       String s = str.substring(0, i + 1);
+    if ( isPalindrome(s) )
       generate(str.substring(i + 1), ans + s + "|");
     }
   }
