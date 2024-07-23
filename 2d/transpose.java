3e65ca150a6407774ae6960f01801948c5e979bd
Anmol Jhamb <talktoanmol@outlook.com>
2024-07-23 15:20:41 +0530

    transpose

--- 2d/transpose.java
+++ 2d/transpose.java
@@ -13,7 +13,7 @@ public class transpose {
     display(arr);
 
     for (int i = 0; i < arr.length; i++) {
-      for (int j = 0; j < arr.length; j++) {
+      for (int j = 0; j < i; j++) {
         int temp = arr[i][j];
         arr[i][j] = arr[j][i];
         arr[j][i] = temp;
