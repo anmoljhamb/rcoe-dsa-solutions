c1625ffef1a73d017770e567c5f5cf2f79e58463
Anmol Jhamb <talktoanmol@outlook.com>
2024-07-23 15:20:27 +0530

    2d transpose

---
+++ 2d/transpose.java
@@ -0,0 +1,35 @@
+public class transpose {
+  public static void main(String[] args) {
+    int n = 3;
+    int m = 3;
+
+    int[][] arr = {
+      {1, 2, 3},
+      {4, 5, 6},
+      {7, 8, 9},
+    };
+
+    System.out.println("Before Transpose: ");
+    display(arr);
+
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = 0; j < arr.length; j++) {
+        int temp = arr[i][j];
+        arr[i][j] = arr[j][i];
+        arr[j][i] = temp;
+      }
+    }
+
+    System.out.println("After transpose: ");
+    display(arr);
+  }
+
+  static void display(int[][] arr) {
+    for (int i = 0; i < arr.length; i++) {
+      for (int j = 0; j < arr[0].length; j++) {
+        System.out.print(arr[i][j] + " ");
+      }
+      System.out.println();
+    }
+  }
+}
