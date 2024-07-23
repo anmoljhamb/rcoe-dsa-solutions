d80e66c7244ab374b0ce874b67cab47f4957944f
Anmol Jhamb <talktoanmol@outlook.com>
2024-07-23 11:23:04 +0530

    formatter added

--- recursion/partitions.java
+++ recursion/partitions.java
@@ -17,17 +17,15 @@ public class partitions {
     return true;
   }
 
-  
-  static void generate(String str, String ans){
-  if (str.length() == 0) {
+  static void generate(String str, String ans) {
+    if (str.length() == 0) {
 
-    System.out.println(ans);
+      System.out.println(ans);
       return;
     }
     for (int i = 0; i < str.length(); i++) {
       String s = str.substring(0, i + 1);
-    if ( isPalindrome(s) )
-      generate(str.substring(i + 1), ans + s + "|");
+      if (isPalindrome(s)) generate(str.substring(i + 1), ans + s + "|");
     }
   }
 }
