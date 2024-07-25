426ed8697b9744ccce7879324b00be2656dfeb7a
Anmol Jhamb <talktoanmol@outlook.com>
2024-07-25 15:08:48 +0530

    factorial

--- recursion/sem3day8.java
+++ recursion/sem3day8.java
@@ -2,7 +2,13 @@ public class sem3day8 {
   public static void main(String[] args) {
     int n = 5;
     // printFromN(n);
-    printToN(n);
+    // printToN(n);
+    System.out.println(factorial(5));
+  }
+
+  static int factorial(int n) {
+    if (n == 0) return 1;
+    return n * factorial(n - 1);
   }
 
   static void printToN(int n) {
