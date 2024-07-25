6c8fc4d08d628f1f3d13055d8708f06c5047555c
Anmol Jhamb <talktoanmol@outlook.com>
2024-07-25 15:28:21 +0530

    sem3day8

--- recursion/sem3day8.java
+++ recursion/sem3day8.java
@@ -4,7 +4,38 @@ public class sem3day8 {
     // printFromN(n);
     // printToN(n);
     // System.out.println(factorial(5));
-    factorial2(n, 1);
+    // factorial2(n, 1);
+    int a = 2;
+    int b = 10;
+    System.out.println(pow(a, b));
+    pow1(a, b);
+    pow(a, b, 1);
+  }
+
+  static void pow(int a, int b, int ans) {
+    if (b == 0) {
+      System.out.println(ans);
+      return;
+    }
+    pow(a, b - 1, a * ans);
+  }
+
+  static int ans = 1;
+
+  static void pow1(int a, int b) {
+    if (b == 0) {
+      System.out.println(ans);
+      return;
+    }
+    ans = ans * a;
+    pow1(a, b - 1);
+  }
+
+  static int pow(int a, int b) {
+    if (b == 0) {
+      return 1;
+    }
+    return a * pow(a, b - 1);
   }
 
   static void factorial2(int n, int ans) {
@@ -14,8 +45,6 @@ public class sem3day8 {
     }
     factorial2(n - 1, ans * n);
   }
-
-  static int ans = 1;
 
   static void factorial1(int n) {
     if (n == 0) {
