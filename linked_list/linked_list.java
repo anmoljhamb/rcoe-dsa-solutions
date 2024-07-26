f28e2bc511582982de1e4cd93ff9a1e70055add3
Anmol Jhamb <talktoanmol@outlook.com>
2024-07-26 13:04:17 +0530

    fix cycle

--- linked_list/linked_list.java
+++ linked_list/linked_list.java
@@ -131,7 +131,7 @@ class LinkedList {
     Node fast = head;
     while (fast != null && fast.next != null) {
       fast = fast.next.next;
-      slow = slow.next.next;
+      slow = slow.next;
       if (slow == fast) {
         return true;
       }
