7a9f13e8452ca078b4723ae704bc8f3004ed2503
Anmol Jhamb <talktoanmol@outlook.com>
2024-07-26 15:43:50 +0530

    mazepath

--- recursion/sem3day9.java
+++ recursion/sem3day9.java
@@ -1,7 +1,20 @@
 public class sem3day9 {
   public static void main(String[] args) {
     int k = 7;
-    boardPath(4, "", k);
+    // boardPath(4, "", k);
+    mazePath(3, 3, 0, 0, "");
+  }
+
+  public static void mazePath(int m, int n, int row, int col, String ans) {
+    if (row >= m || col >= n) {
+      return;
+    }
+    if (row == m - 1 && col == n - 1) {
+      System.out.println(ans);
+      return;
+    }
+    mazePath(m, n, row + 1, col, ans + "D");
+    mazePath(m, n, row, col + 1, ans + "R");
   }
 
   public static void boardPath(int target, String ans, int k) {
